58702e2eb98dfb8fbb0cb5a854be4defb4915d96
Emerson Lara <snowbg@gmail.com>
2018-07-01 18:38:58 -0300

    Method to create a job.
    Changed for a method calling.
    create_job()

--- vd_converter.py
+++ vd_converter.py
@@ -1,16 +1,18 @@
 import requests as rq
 import json
-url = "https://app.zencoder.com/api/v2/jobs/"
-key = "cef97ec9aa4f0ed89b2c7c128c3bee98"
-headers = {"Zencoder-Api-Key":key, "Content-Type":"application/json"}
-payload = {"test": "true","input": "s3://zencodertesting/test.mov"}
 
-sts = rq.get(url, headers = headers)
-print(sts)
-print(headers)
-print(payload)
-r = rq.post(url, headers = headers, data = json.dumps(payload))
-print(r)
-print("HTTP Status:", r.status_code)
-print("JSON: ", r.json())
+key="cef97ec9aa4f0ed89b2c7c128c3bee98"
+url = "https://app.zencoder.com/api/v2/jobs/"
+def create_job(url = url, key= key,
+              headers = {"Zencoder-Api-Key":key, "Content-Type":"application/json"},
+              payload = {"test": "true","input": "s3://zencodertesting/test.mov"}):
+    sts = rq.get(url, headers = headers)
+    r = rq.post(url, headers = headers, data = json.dumps(payload))
+    print("HTTP Status:", r.status_code)
+    print("JSON: ", r.json())
+    return r.json()
+
+
+if __name__ == '__main__':
+    job_json = create_job()
 
