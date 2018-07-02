e85ff399cd2262afdbc06a8600797dd2ba8c4f3b
Emerson Lara <snowbg@gmail.com>
2018-07-01 21:33:20 -0300

    Added boto3 upload for Amazon S3
    added a method that uploads the video file to Amazon S3.

--- vd_converter.py
+++ vd_converter.py
@@ -1,8 +1,13 @@
 import requests as rq
 import json
+from tkinter import askopenfilename, askdirectory
+import boto3
 
 key="cef97ec9aa4f0ed89b2c7c128c3bee98"
 url = "https://app.zencoder.com/api/v2/jobs/"
+video = askopenfilename()
+
+
 def create_job(url = url, key= key,
               headers = {"Zencoder-Api-Key":key, "Content-Type":"application/json"},
               payload = {"test": "true","input": "s3://zencodertesting/test.mov"}):
@@ -11,6 +16,12 @@ def create_job(url = url, key= key,
     print("HTTP Status:", r.status_code)
     print("JSON: ", r.json())
     return r.json()
+
+def upload_video(video):
+    s3 = boto3.resource('s3')
+    with open(video, 'rb') as vd:
+        s3.bucket(my-bucket).put_object(key=video, Body=vd)
+
 
 
 if __name__ == '__main__':
